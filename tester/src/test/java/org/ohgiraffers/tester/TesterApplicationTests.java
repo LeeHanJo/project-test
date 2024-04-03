c486b7a0ac212f06ab12d5334554e0a6fea239e6
LeeHanJo <gps95wwkhhev@gmail.com>
2024-04-03 13:09:46 +0900

    -first commit Test -

---
+++ tester/src/test/java/org/ohgiraffers/tester/TesterApplicationTests.java
@@ -0,0 +1,13 @@
+package org.ohgiraffers.tester;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TesterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
